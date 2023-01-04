ebba68217d25bed1445e22db365e6d35839de18a
ZanZver <zan.zver98@gmail.com>
2023-01-04 22:59:10 +0000

    Updated GetParkingToBePaid procedure to accept ID
    Now GetParkingToBePaid procedure accepts ID on which filtering can be done

--- Code/SQL/DDL/PROCEDURE.sql
+++ Code/SQL/DDL/PROCEDURE.sql
@@ -4,16 +4,34 @@
 ================================================================================================================================================
 */
 
-CREATE DEFINER=`my_user`@`%` PROCEDURE `Airport_DB`.`GetParkingToBePaid`()
+
+/*
+================================================================================================================================================
+        Create procedure
+================================================================================================================================================
+*/
+CREATE DEFINER=`my_user`@`%` 
+PROCEDURE `Airport_DB`.`GetParkingToBePaid2` (Filter_Customer_ID int(11))
 BEGIN
 	SELECT c.Customer_ID
-       , pt.Token_ID
-       , pt.`Date`
-       , cps.*
-       , cps.Price_Per_Hour * (SELECT TIMESTAMPDIFF(HOUR, pt.`Date` , CURRENT_TIMESTAMP) * (30 / 60) ) as ToPay
+          , pt.Token_ID
+          , pt.`Date`
+          , cps.*
+          , cps.Price_Per_Hour * (SELECT TIMESTAMPDIFF
+            (HOUR, pt.`Date` , CURRENT_TIMESTAMP)
+            * (30 / 60) ) as ToPay
 	FROM Customer c
 	INNER JOIN ParkingToken pt 
 	ON c.Token_FK = pt.Token_ID
 	INNER JOIN CustomerParkingSpot cps 
-	ON cps.Parking_Spot_ID = pt.Token_ID;
+	ON cps.Parking_Spot_ID = pt.Token_ID
+	WHERE c.Customer_ID = Filter_Customer_ID;
 END;
+
+/*
+================================================================================================================================================
+        Call procedure
+================================================================================================================================================
+*/
+-- Replace Customer_ID with desired ID
+CALL Airport_DB.GetParkingToBePaid2(Customer_ID);
